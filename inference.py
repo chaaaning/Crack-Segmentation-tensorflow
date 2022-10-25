2a30e5d2cfb9b4038b6c7a26cc25539beffd32d7
chaaaning <yunjch21@gmail.com>
2022-10-25 15:31:16 +0900

    modify inference.py --to save making result

--- inference.py
+++ inference.py
@@ -11,6 +11,7 @@ from utils.utils import load_image, decode_one_hot
 from utils.helpers import get_dataset_info
 from keras_applications import imagenet_utils
 from builders import builder
+from scipy.sparse import csr_matrix
 from PIL import Image
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -39,14 +40,17 @@ parser.add_argument('--is_save', help='save options.', type=bool, default=False)
 args = parser.parse_args()
 
 ### 필요한 함수 정의 ###
-# 1. image와 prediction 합치기
-def merge_img(frame, pred, img_size):
+# -- 1. image와 prediction 합치기
+def merge_img(frame, pred, img_size, is_BGR=True):
     re_frm = cv2.resize(frame, dsize=img_size, interpolation=cv2.INTER_CUBIC)
-    add_mask = re_frm.copy()
+    if is_BGR:
+        add_mask = cv2.cvtColor(re_frm, cv2.COLOR_BGR2RGB)
+    else:
+        add_mask = re_frm.copy()
     add_mask[pred[:,:]!=0]=[0,255,0]
     return add_mask
 
-# 2. 추가 데이터 수집 로직을 위한 json Load
+# -- 2. 추가 데이터 수집 로직을 위한 json Load
 def json_to_df(json_path, save=False):
     with open(json_path, "r") as f:
         json_data = json.load(f)
@@ -82,84 +86,109 @@ def json_to_df(json_path, save=False):
     if save:
         json_df.to_csv(os.path.basename(json_path).rstrip(".json")+".csv", index=False)
         
-    return json_df     
+    return json_df
+
+# -- 3. masking rate calculate    
+def calc_masking_rate(arr, width, height):
+    masking_val = arr.sum()
+    return (masking_val*100)/(width*height)
+
 #######################
+## Keyboard Interrupt가 발생하면 저장할 수 있도록 코드 라인 (try-except) 형성
+try:
+    # build the model
+    net, base_model = builder(args.num_classes, (args.crop_height, args.crop_width), args.model, args.base_model)
 
-# build the model
-net, base_model = builder(args.num_classes, (args.crop_height, args.crop_width), args.model, args.base_model)
+    # load weights
+    print('Loading the weights...')
+    if args.weights is not None:
+        print(os.getcwd())
+        net.load_weights(filepath=args.weights)
+    else:
+        if not os.path.exists(args.weights):
+            raise ValueError('The weights file does not exist in \'{path}\''.format(path=args.weights))
+        net.load_weights(args.weights)
 
-# load weights
-print('Loading the weights...')
-if args.weights is not None:
-    print(os.getcwd())
-    net.load_weights(filepath=args.weights)
-else:
-    if not os.path.exists(args.weights):
-        raise ValueError('The weights file does not exist in \'{path}\''.format(path=args.weights))
-    net.load_weights(args.weights)
+    # begin testing
+    print("\n***** Begin testing *****")
+    print("Model -->", args.model)
+    print("Base Model -->", base_model)
+    print("Crop Height -->", args.crop_height)
+    print("Crop Width -->", args.crop_width)
+    print("Num Classes -->", args.num_classes)
 
-# begin testing
-print("\n***** Begin testing *****")
-print("Model -->", args.model)
-print("Base Model -->", base_model)
-print("Crop Height -->", args.crop_height)
-print("Crop Width -->", args.crop_width)
-print("Num Classes -->", args.num_classes)
+    print("")
 
-print("")
+    # load_images & json
+    df_json = json_to_df(args.json_path)
+    image_names = df_json["name"]+"."+df_json["format"]
+    image_names = image_names.apply(lambda x: os.path.basename(x))
 
-# load_images & json
-df_json = json_to_df(args.json_path)
-image_names = df_json["name"]+"."+df_json["format"]
-image_names = image_names.apply(lambda x: os.path.basename(x))
+    # make ETL pipeline initialization -> output json
+    # 들어 가야할 내용 : 'path', 'name', 'format', 'latitude', 'longitude'
+    # key_list = ['path', 'name', 'format', 'latitude', 'longitude']
+    # data_extract = dict()
+    # for key in key_list:    # dictionary 초기화 (json을 어떻게 넘길까 고민,,)
+    #     data_extract[key] = []
 
-# make ETL pipeline initialization -> output json
-# 들어 가야할 내용 : 'path', 'name', 'format', 'latitude', 'longitude'
-# key_list = ['path', 'name', 'format', 'latitude', 'longitude']
-# data_extract = dict()
-# for key in key_list:    # dictionary 초기화 (json을 어떻게 넘길까 고민,,)
-#     data_extract[key] = []
+    mask_indptr, mask_indices, masking_rates = [], [], []
+    # predict & save
+    for i, img_name in enumerate(image_names):
 
-# predict & save
-for i, img_name in enumerate(image_names):
-    sys.stdout.write('\rRunning test image %d / %d'%(i+1, len(image_names)))
-    sys.stdout.flush()
+        # loading image & convert dim
+        img_pname = args.image_path+"\\"+img_name.split(".")[0]+"\\"+img_name
+        init_image = cv2.resize(load_image(img_pname), dsize=(args.crop_width, args.crop_height))
+        image = imagenet_utils.preprocess_input(init_image.astype('float32'), data_format='channels_last', mode='torch')
 
-    # loading image & convert dim
-    img_pname = args.image_path+"\\"+img_name.split(".")[0]+"\\"+img_name
-    init_image = cv2.resize(load_image(img_pname), dsize=(args.crop_width, args.crop_height))
-    image = imagenet_utils.preprocess_input(init_image.astype('float32'), data_format='channels_last', mode='torch')
+        if np.ndim(image) == 3:
+            image = np.expand_dims(image, axis=0)
+        assert np.ndim(image) == 4
 
-    if np.ndim(image) == 3:
-        image = np.expand_dims(image, axis=0)
-    assert np.ndim(image) == 4
-
-    preds = net.predict(image)
-    preds = tf.nn.softmax(preds)
-    
-    if np.ndim(preds) == 4:
-        prediction = np.squeeze(preds, axis=0)
-    
-    np_prediction = decode_one_hot(prediction)
-    prediction = Image.fromarray(np.uint8(prediction))
-    
-    # save the prediction
-    if args.is_save:
-        if i==0:
-            _, file_name = os.path.split(img_name)
-            if os.path.exists(args.save_path):
-                save_path = args.save_path
-            else:
-                save_path = os.mkdir(os.path.join(os.getcwd(), 'predictions'))
-                print("make prediction save path !")
-        ## Save(1) - PIL save
-        # prediction.save(os.path.join(save_path, img_name))
+        preds = net.predict(image)
+        preds = tf.nn.softmax(preds)
         
-        ## Save(2) - pyplot save
-        # plt.imshow(prediction)
-        # plt.savefig(os.path.join(save_path, img_name))
+        if np.ndim(preds) == 4:
+            prediction = np.squeeze(preds, axis=0)
         
-        ## Save(3) - Image merge save
-        img_mask_merge = merge_img(init_image, np_prediction, (args.crop_width, args.crop_height))
-        cv2.imwrite(os.path.join(save_path, img_name), img_mask_merge)
+        np_prediction = decode_one_hot(prediction)
+        # prediction = Image.fromarray(np.uint8(np_prediction))
         
+        csr_mat = csr_matrix(np_prediction)
+        mask_indptr.append(csr_mat.indptr.tolist())
+        mask_indices.append(csr_mat.indices.tolist())
+        masking_rates.append(calc_masking_rate(np_prediction, args.crop_width, args.crop_height))
+        
+        # save the prediction
+        if args.is_save:
+            if i==0:
+                _, file_name = os.path.split(img_name)
+                if os.path.exists(args.save_path):
+                    save_path = args.save_path
+                else:
+                    save_path = os.path.join(os.getcwd(), 'predictions')
+                    os.mkdir(save_path)
+                    print("make prediction save path !")
+
+            img_mask_merge = merge_img(init_image, np_prediction, (args.crop_width, args.crop_height))
+            cv2.imwrite(os.path.join(save_path, img_name), img_mask_merge) 
+        
+        sys.stdout.write('\rRunning test image %d / %d'%(i+1, len(image_names)))
+        sys.stdout.flush()
+        
+    save_df = df_json.iloc[:,:].copy()
+    save_df["mask_indptr"] = mask_indptr
+    save_df["mask_indices"] = mask_indices
+    save_df["masking_rate"] = masking_rates
+    save_df.to_csv(os.path.join(save_path, "masking_result.csv"), index=False)
+    
+        
+except KeyboardInterrupt:
+    img_mask_merge = merge_img(init_image, np_prediction, (args.crop_width, args.crop_height))
+    cv2.imwrite(os.path.join(save_path, img_name), img_mask_merge)
+    
+    print(f"Keyboard Interrupt !! save result data ...")
+    save_df = df_json.iloc[:i,:].copy()
+    save_df["mask_indptr"] = mask_indptr
+    save_df["mask_indices"] = mask_indices
+    save_df["masking_rate"] = masking_rates
+    save_df.to_csv(os.path.join(save_path, "masking_result.csv"), index=False)
